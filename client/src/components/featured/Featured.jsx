b78d649141ed560040ea6994d8be4194c0d5c58c
Doriana <doryana2001@yahoo.com>
2024-04-08 16:46:39 +0300

    Initial commit

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,55 @@
+import useFetch from "../../hooks/useFetch";
+import "./featured.css";
+
+const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrid,london"
+  );
+
+  return (
+    <div className="featured">
+      {loading ? (
+        "Loading please wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://images.unsplash.com/photo-1528728329032-2972f65dfb3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} properties</h2>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://images.unsplash.com/photo-1543783207-ec64e4d95325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Madrid</h1>
+              <h2>{data[1]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://images.unsplash.com/photo-1520986606214-8b456906c813?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>London</h1>
+              <h2>{data[2]} properties</h2>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
---
+++ client/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,40 @@
+import useFetch from "../../hooks/useFetch";
+import "./propertyList.css";
+
+const PropertyList = () => {
+  const { data, loading, error } = useFetch("/hotels/countByType");
+
+  const images = [
+    "https://images.unsplash.com/photo-1596436889106-be35e843f974?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1615880484746-a134be9a6ecf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "https://images.unsplash.com/photo-1631630259742-c0f0b17c6c10?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  ];
+  return (
+    <div className="pList">
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img,i) => (
+              <div className="pListItem" key={i}>
+                <img
+                  src={img}
+                  alt=""
+                  className="pListImg"
+                />
+                <div className="pListTitles">
+                  <h1>{data[i]?.type}</h1>
+                  <h2>{data[i]?.count} {data[i]?.type}</h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PropertyList;
